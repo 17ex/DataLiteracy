5e141a7f93adebf5ff8e07bbfc54f72a021596b7
Fabian-Mor <fabian.morelli@student.uni-tuebingen.de>
2023-12-10 19:02:50 +0100

    Added basic plotting of the reachable transfers.

--- analysis.py
+++ analysis.py
@@ -1,5 +1,7 @@
 import pandas as pd
+import numpy as np
 import pickle
+import matplotlib.pyplot as plt
 
 with open('data/incoming.pkl', 'rb') as file:
     incoming = pickle.load(file)
@@ -41,3 +43,35 @@ sorted_items = sorted(reachable_count.items())
 # Displaying the sorted list of tuples
 for key, value in sorted_items:
     print(f"Key: {key}, Value: {value}")
+
+# Filter the data for keys smaller than 100 and sort by keys
+filtered_data = [(key, value) for key, value in sorted_items if key < 100]
+filtered_data.sort(key=lambda x: x[0])  # Sort by keys
+
+# Calculate percentages for 'reachable' and 'not_reachable'
+percentages_reachable = [(key, (value['reachable'] / (value['reachable'] + value['not_reachable'])) * 100) for key, value in filtered_data]
+percentages_not_reachable = [(key, (value['not_reachable'] / (value['reachable'] + value['not_reachable'])) * 100) for key, value in filtered_data]
+
+# Create bins and calculate average percentages for each bin
+num_bins = int(max([key for key, _ in filtered_data]) / 5) + 1
+bins = [[] for _ in range(num_bins)]
+
+for i, (key, percentage) in enumerate(percentages_reachable):
+    bin_index = int(key / 5)
+    bins[bin_index].append(percentage)
+
+average_reachable_percentages = [np.mean(bin_values) for bin_values in bins if bin_values]
+
+# Create labels for bins
+bin_labels = [f"{5 * i}-{5 * (i + 1) - 1}" for i in range(num_bins) if bins[i]]
+
+# Plotting the average percentages per bin
+plt.figure(figsize=(10, 6))
+
+plt.bar(bin_labels, average_reachable_percentages, color='blue')
+plt.xlabel('Planned transfer time')
+plt.ylabel('Percentage of reachable transfers')
+plt.title('Percentage of reachable transfers given the planned transfer time')
+plt.xticks(rotation=45, ha='right')
+plt.tight_layout()
+plt.show()
